e8c27b070c2a591ec6fe576de48316943eff60eb
Dave Beckett <dave@dajobe.org>
2007-02-18 07:44:12 +0000

    (flickcurl_free_context, flickcurl_free_contexts, flickcurl_build_contexts): Added

--- flickcurl.c
+++ flickcurl.c
@@ -622,6 +622,33 @@ flickcurl_free_person(flickcurl_person *person)
 }
 
 
+void
+flickcurl_free_context(flickcurl_context *context)
+{
+  if(context->id)
+    free(context->id);
+  if(context->secret)
+    free(context->secret);
+  if(context->title)
+    free(context->title);
+  if(context->url)
+    free(context->url);
+  if(context->thumb)
+    free(context->thumb);
+  free(context);
+}
+
+
+void
+flickcurl_free_contexts(flickcurl_context** contexts)
+{
+  int i;
+  for(i=0; contexts[i]; i++)
+    flickcurl_free_context(contexts[i]);
+  free(contexts);
+}
+
+
 char*
 flickcurl_unixtime_to_isotime(time_t unix_time)
 {
@@ -723,4 +750,87 @@ flickcurl_photo_as_source_uri(flickcurl_photo *photo, const char c)
   result=(char*)malloc(len+1);
   strcpy(result, buf);
   return result;
+}
+
+
+flickcurl_context**
+flickcurl_build_contexts(flickcurl* fc, xmlDocPtr doc)
+{
+  flickcurl_context** contexts=NULL;
+  xmlNodePtr xnp;
+  xmlNodePtr node;
+  int i;
+  int count=0;
+  
+  contexts=(flickcurl_context**)calloc(sizeof(flickcurl_context**), 3);
+
+  /* walk children elements of root element */
+  xnp = xmlDocGetRootElement(doc);
+  for(i=0, node=xnp->children;
+      node;
+      i++, node=node->next) {
+    xmlAttr* attr;
+    flickcurl_context* context;
+    flickcurl_context_type type=FLICKCURL_CONTEXT_NONE;
+    int j;
+    
+#if FLICKCURL_DEBUG > 1
+    fprintf(stderr, "XML node name %s XML type %d\n", node->name, node->type);
+#endif
+    
+    if(node->type != XML_ELEMENT_NODE)
+      continue;
+
+    for(j=0; j <= FLICKCURL_CONTEXT_LAST; j++) {
+      if(!strcmp((const char*)node->name, flickcurl_context_type_element[j])) {
+        type=(flickcurl_context_type)j;
+        break;
+      }
+    }
+    if(type == FLICKCURL_CONTEXT_NONE)
+      continue;
+    
+    context=(flickcurl_context*)calloc(sizeof(flickcurl_context), 1);
+    context->type=type;
+
+    for(attr=node->properties; attr; attr=attr->next) {
+      const char *attr_name=(const char*)attr->name;
+      char *attr_value;
+      
+      attr_value=(char*)malloc(strlen((const char*)attr->children->content)+1);
+      strcpy(attr_value, (const char*)attr->children->content);
+      
+      if(!strcmp(attr_name, "id"))
+        context->id=attr_value;
+      else if(!strcmp(attr_name, "secret"))
+        context->secret=attr_value;
+      else if(!strcmp(attr_name, "server"))
+        context->server=atoi(attr_value);
+      else if(!strcmp(attr_name, "farm"))
+        context->farm=atoi(attr_value);
+      else if(!strcmp(attr_name, "title"))
+        context->title=attr_value;
+      else if(!strcmp(attr_name, "url"))
+        context->url=attr_value;
+      else if(!strcmp(attr_name, "thumb"))
+        context->thumb=attr_value;
+    } /* for attributes */
+    
+#if FLICKCURL_DEBUG > 1
+    fprintf(stderr, "context: id %s secret %s server %d farm %d title '%s'\n  url '%s'\n  thumb '%s'\n",
+            context->id, 
+            (context->secret ? context->secret : "NULL"),
+            context->server, context->farm,
+            (context->title ? context->title : "NULL"),
+            context->url,
+            context->thumb
+            );
+#endif
+    
+    contexts[count++]=context;
+  } /* for nodes */
+
+  contexts[count]=NULL;
+
+  return contexts;
 }
