ccd34ca68ad35084697fa5ae05addb5f5f78419a
Dave Beckett <dave@dajobe.org>
2007-02-19 00:00:07 +0000

    docs

--- urls-api.c
+++ urls-api.c
@@ -1,6 +1,6 @@
 /* -*- Mode: c; c-basic-offset: 2 -*-
  *
- * api.c - Flickr API calls
+ * urls-api.c - Flickr urls API calls
  *
  * Copyright (C) 2007, David Beckett http://purl.org/net/dajobe/
  * 
