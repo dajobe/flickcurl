82aa4eef0abfabc34297620ddc2a7474af9a2b5d
Dave Beckett <dave@dajobe.org>
2007-02-26 04:26:44 +0000

    (flickcurl_photos_addTags, flickcurl_photos_delete, flickcurl_photos_removeTag, flickcurl_photos_setTags): Added

--- photos-api.c
+++ photos-api.c
@@ -38,16 +38,88 @@
 #include <flickcurl_internal.h>
 
 
-
-
-/*
- * flickr.photos.addTags
+/**
+ * flickcurl_photos_addTags:
+ * @fc: flickcurl context
+ * @photo_id: photo ID
+ * @tags: tags to add as a space-separated list
+ *
+ * Add tags to a photo.
+ *
+ * Implements flickr.photos.addTags (0.9)
  */
+int
+flickcurl_photos_addTags(flickcurl* fc, const char* photo_id, const char* tags)
+{
+  const char * parameters[7][2];
+  int count=0;
+  xmlDocPtr doc=NULL;
+  
+  if(!photo_id || !tags)
+    return 1;
+  
+  parameters[count][0]  = "photo_id";
+  parameters[count++][1]= photo_id;
+
+  parameters[count][0]  = "tags";
+  parameters[count++][1]= tags;
+    
+  parameters[count][0]  = NULL;
+
+  if(flickcurl_prepare(fc, "flickr.photos.addTags", parameters, count))
+    goto tidy;
+  
+  flickcurl_set_write(fc, 1);
+  flickcurl_set_data(fc, (void*)"", 0);
+    
+  doc=flickcurl_invoke(fc);
+  if(!doc)
+    goto tidy;
+
+ tidy:
+
+  return fc->failed;
+}
 
 
-/*
- * flickr.photos.delete
+/**
+ * flickcurl_photos_delete:
+ * @fc: flickcurl context
+ * @photo_id: photo ID
+ *
+ * Delete a photo.
+ *
+ * Implements flickr.photos.delete (0.9)
  */
+int
+flickcurl_photos_delete(flickcurl* fc, const char* photo_id)
+{
+  const char * parameters[6][2];
+  int count=0;
+  xmlDocPtr doc=NULL;
+  
+  if(!photo_id)
+    return 1;
+  
+  parameters[count][0]  = "photo_id";
+  parameters[count++][1]= photo_id;
+
+  parameters[count][0]  = NULL;
+
+  if(flickcurl_prepare(fc, "flickr.photos.delete", parameters, count))
+    goto tidy;
+  
+  flickcurl_set_write(fc, 1);
+  flickcurl_set_data(fc, (void*)"", 0);
+    
+  doc=flickcurl_invoke(fc);
+  if(!doc)
+    goto tidy;
+
+ tidy:
+
+  return fc->failed;
+}
 
 
 /**
@@ -256,9 +328,46 @@ flickcurl_photos_getInfo(flickcurl* fc, const char* photo_id)
  */
 
 
-/*
- * flickr.photos.removeTag
+/**
+ * flickcurl_photos_removeTag:
+ * @fc: flickcurl context
+ * @tag_id: tag ID to remove from the photo
+ *
+ * Remove a tag from a photo.
+ *
+ * The @tag_id is returned such as from flickr_photos_getInfo()
+ *
+ * Implements flickr.photos.removeTag (0.9)
  */
+int
+flickcurl_photos_removeTag(flickcurl* fc, const char* tag_id)
+{
+  const char * parameters[6][2];
+  int count=0;
+  xmlDocPtr doc=NULL;
+  
+  if(!tag_id)
+    return 1;
+  
+  parameters[count][0]  = "tag_id";
+  parameters[count++][1]= tag_id;
+    
+  parameters[count][0]  = NULL;
+
+  if(flickcurl_prepare(fc, "flickr.photos.removeTag", parameters, count))
+    goto tidy;
+  
+  flickcurl_set_write(fc, 1);
+  flickcurl_set_data(fc, (void*)"", 0);
+    
+  doc=flickcurl_invoke(fc);
+  if(!doc)
+    goto tidy;
+
+ tidy:
+
+  return fc->failed;
+}
 
 
 /*
@@ -281,6 +390,47 @@ flickcurl_photos_getInfo(flickcurl* fc, const char* photo_id)
  */
 
 
-/*
- * flickr.photos.setTags
+/**
+ * flickcurl_photos_setTags:
+ * @fc: flickcurl context
+ * @photo_id: photo ID
+ * @tags: all tags for the photo as a space-separated list
+ *
+ * Set the tags for a photo.
+ *
+ * Note that this replaces all existing tags with the @tags here.
+ *
+ * Implements flickr.photos.setTags (0.9)
  */
+int
+flickcurl_photos_setTags(flickcurl* fc, const char* photo_id, const char* tags)
+{
+  const char * parameters[7][2];
+  int count=0;
+  xmlDocPtr doc=NULL;
+  
+  if(!photo_id || !tags)
+    return 1;
+  
+  parameters[count][0]  = "photo_id";
+  parameters[count++][1]= photo_id;
+    
+  parameters[count][0]  = "tags";
+  parameters[count++][1]= tags;
+    
+  parameters[count][0]  = NULL;
+
+  if(flickcurl_prepare(fc, "flickr.photos.setTags", parameters, count))
+    goto tidy;
+  
+  flickcurl_set_write(fc, 1);
+  flickcurl_set_data(fc, (void*)"", 0);
+    
+  doc=flickcurl_invoke(fc);
+  if(!doc)
+    goto tidy;
+
+ tidy:
+
+  return fc->failed;
+}
