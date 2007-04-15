07de9855bc11227f2744ad1d56f1199207f0221a
Dave Beckett <dave@dajobe.org>
2007-04-15 06:16:16 +0000

    Added xsd

--- flickrdf.c
+++ flickrdf.c
@@ -164,6 +164,7 @@ static struct {
   { "rdfs",     "http://www.w3.org/2000/01/rdf-schema#" },
   { "skos",     "http://www.w3.org/2004/02/skos/core" },
   { "upcoming", "http://www.machinetags.org/wiki/Upcoming#" },
+  { "xsd",      "http://www.w3.org/2001/XMLSchema#" },
   { NULL, NULL }
 };
 
