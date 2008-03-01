8ad62647a6113714a914f542fdd69056d8b804f9
Dave Beckett <dave@dajobe.org>
2008-03-01 04:52:51 +0000

    Add AC_HEADER_TIME standed ifdef headers to get time structs and prototypes.

--- src/common.c
+++ src/common.c
@@ -37,8 +37,15 @@
 #ifdef HAVE_ERRNO_H
 #include <errno.h>
 #endif
-#ifdef HAVE_TIME_H
-#include <time.h>
+#if TIME_WITH_SYS_TIME
+# include <sys/time.h>
+# include <time.h>
+#else
+# if HAVE_SYS_TIME_H
+#  include <sys/time.h>
+# else
+#  include <time.h>
+# endif
 #endif
 
 #include <flickcurl.h>
