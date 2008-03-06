cf0c55467985048f19046be6fba470c714e4d7d4
Dave Beckett <dave@dajobe.org>
2008-03-06 05:18:04 +0000

    Added list-methods example utility

---
+++ utils/list-methods.c
@@ -0,0 +1,293 @@
+/* -*- Mode: c; c-basic-offset: 2 -*-
+ *
+ * list-methods utility - List all Flickr API methods via reflection
+ *
+ * Copyright (C) 2008, David Beckett http://www.dajobe.org/
+ * 
+ * This file is licensed under the following three licenses as alternatives:
+ *   1. GNU Lesser General Public License (LGPL) V2.1 or any newer version
+ *   2. GNU General Public License (GPL) V2 or any newer version
+ *   3. Apache License, V2.0 or any newer version
+ * 
+ * You may not use this file except in compliance with at least one of
+ * the above three licenses.
+ * 
+ * See LICENSE.html or LICENSE.txt at the top of this package for the
+ * complete terms and further detail along with the license texts for
+ * the licenses in COPYING.LIB, COPYING and LICENSE-2.0.txt respectively.
+ * 
+ *
+ * USAGE: list-methods [OPTIONS]
+ *
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <stdarg.h>
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#ifdef HAVE_STDLIB_H
+#include <stdlib.h>
+#endif
+#ifdef HAVE_UNISTD_H
+#include <unistd.h>
+#endif
+#ifdef HAVE_ERRNO_H
+#include <errno.h>
+#endif
+
+/* many places for getopt */
+#ifdef HAVE_GETOPT_H
+#include <getopt.h>
+#else
+#include <flickcurl_getopt.h>
+#endif
+
+#include <flickcurl.h>
+
+
+
+#ifdef NEED_OPTIND_DECLARATION
+extern int optind;
+extern char *optarg;
+#endif
+
+
+static const char* program;
+
+static const char*
+my_basename(const char *name)
+{
+  char *p;
+  if((p=strrchr(name, '/')))
+    name=p+1;
+  else if((p=strrchr(name, '\\')))
+    name=p+1;
+
+  return name;
+}
+
+
+static void
+my_message_handler(void *user_data, const char *message)
+{
+  fprintf(stderr, "%s: ERROR: %s\n", program, message);
+}
+
+
+static void
+my_set_config_var_handler(void* userdata, const char* key, const char* value)
+{
+  flickcurl *fc=(flickcurl *)userdata;
+  
+  if(!strcmp(key, "api_key"))
+    flickcurl_set_api_key(fc, value);
+  else if(!strcmp(key, "secret"))
+    flickcurl_set_shared_secret(fc, value);
+  else if(!strcmp(key, "auth_token"))
+    flickcurl_set_auth_token(fc, value);
+}
+
+
+#ifdef HAVE_GETOPT_LONG
+#define HELP_TEXT(short, long, description) "  -" short ", --" long "  " description
+#define HELP_TEXT_LONG(long, description) "      --" long "  " description
+#define HELP_ARG(short, long) "--" #long
+#define HELP_PAD "\n                          "
+#else
+#define HELP_TEXT(short, long, description) "  -" short "  " description
+#define HELP_TEXT_LONG(long, description)
+#define HELP_ARG(short, long) "-" #short
+#define HELP_PAD "\n      "
+#endif
+
+
+#define GETOPT_STRING "hv"
+
+#ifdef HAVE_GETOPT_LONG
+static struct option long_options[] =
+{
+  /* name, has_arg, flag, val */
+  {"help",    0, 0, 'h'},
+  {"version", 0, 0, 'v'},
+  {NULL,      0, 0, 0}
+};
+#endif
+
+
+static const char *title_format_string="List Flickr API methods utility %s\n";
+
+static const char* config_filename=".flickcurl.conf";
+static const char* config_section="flickr";
+
+
+static
+int compare_strings(const void *a, const void *b)
+{
+  return strcmp(*((char**)a), *((char**)b));
+}
+
+
+int
+main(int argc, char *argv[]) 
+{
+  flickcurl *fc=NULL;
+  int rc=0;
+  int usage=0;
+  int help=0;
+  int read_auth=1;
+  int i;
+  const char* home;
+  char config_path[1024];
+  char** methods=NULL;
+  int count=0;
+  
+  flickcurl_init();
+  
+  program=my_basename(argv[0]);
+
+  home=getenv("HOME");
+  if(home)
+    sprintf(config_path, "%s/%s", home, config_filename);
+  else
+    strcpy(config_path, config_filename);
+  
+
+  while (!usage && !help)
+  {
+    int c;
+#ifdef HAVE_GETOPT_LONG
+    int option_index = 0;
+
+    c = getopt_long (argc, argv, GETOPT_STRING, long_options, &option_index);
+#else
+    c = getopt (argc, argv, GETOPT_STRING);
+#endif
+    if (c == -1)
+      break;
+
+    switch (c) {
+      case 0:
+      case '?': /* getopt() - unknown option */
+        usage=1;
+        break;
+
+      case 'h':
+        help=1;
+        break;
+
+      case 'v':
+        fputs(flickcurl_version_string, stdout);
+        fputc('\n', stdout);
+
+        exit(0);
+    }
+    
+  }
+  
+  if(help)
+    goto help;
+  
+  if(argc != 1) {
+    fprintf(stderr, "%s: Extra arguments given\n", program);
+    usage=1;
+  }
+
+  if(usage) {
+    if(usage>1) {
+      fprintf(stderr, title_format_string, flickcurl_version_string);
+      fputs("Flickcurl home page: ", stderr);
+      fputs(flickcurl_home_url_string, stderr);
+      fputc('\n', stderr);
+      fputs(flickcurl_copyright_string, stderr);
+      fputs("\nLicense: ", stderr);
+      fputs(flickcurl_license_string, stderr);
+      fputs("\n\n", stderr);
+    }
+    fprintf(stderr, "Try `%s " HELP_ARG(h, help) "' for more information.\n",
+            program);
+    rc=1;
+    goto tidy;
+  }
+
+  help:
+  if(help) {
+    printf(title_format_string, flickcurl_version_string);
+    puts("List Flickr API methods by reflection.");
+    printf("Usage: %s [OPTIONS]\n\n", program);
+
+    fputs(flickcurl_copyright_string, stdout);
+    fputs("\nLicense: ", stdout);
+    puts(flickcurl_license_string);
+    fputs("Flickcurl home page: ", stdout);
+    puts(flickcurl_home_url_string);
+
+    fputs("\n", stdout);
+
+    puts(HELP_TEXT("h", "help            ", "Print this help, then exit"));
+    puts(HELP_TEXT("v", "version         ", "Print the flickcurl version"));
+
+    rc=0;
+    goto tidy;
+  }
+
+
+  /* Initialise the Flickcurl library */
+  fc=flickcurl_new();
+  if(!fc) {
+    rc=1;
+    goto tidy;
+  }
+
+  flickcurl_set_error_handler(fc, my_message_handler, NULL);
+
+  if(read_auth && !access((const char*)config_path, R_OK)) {
+    if(read_ini_config(config_path, config_section, fc,
+                       my_set_config_var_handler)) {
+      fprintf(stderr, "%s: Failed to read config filename %s: %s\n",
+              program, config_path, strerror(errno));
+      rc=1;
+      goto tidy;
+    }
+  }
+
+  methods=flickcurl_reflection_getMethods(fc);
+  if(!methods) {
+    fprintf(stderr, "%s: getMethods failed\n", program);
+    rc=1;
+    goto tidy;
+  }
+
+  for(i=0; methods[i]; i++)
+    ;
+  count=i;
+  
+  fprintf(stderr, "%s: Found %d API methods\n", program, count);
+  
+  /* it seems to be sorted when returned but ensure it anyway */
+  qsort(methods, count, sizeof(char*), compare_strings);
+  
+  for(i=0; methods[i]; i++) {
+    fputs((const char*)methods[i], stdout);
+    fputc('\n', stdout);
+  }
+
+
+ tidy:
+  if(methods) {
+    for(i=0; methods[i]; i++)
+      free(methods[i]);
+    free(methods);
+    methods=NULL;
+  }
+
+  if(fc)
+    flickcurl_free(fc);
+
+  flickcurl_finish();
+
+  return(rc);
+}
