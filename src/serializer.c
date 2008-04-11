253146d0f7955071aabdc0bbad2c5ea17a9f9de6
Dave Beckett <dave@dajobe.org>
2008-04-11 15:26:57 +0000

    Do not flickcurl_free_photo inside serializer

--- src/serializer.c
+++ src/serializer.c
@@ -522,8 +522,6 @@ flickcurl_serialize_photo(flickcurl_serializer* fcs, flickcurl_photo* photo)
   if(nspaces)
     free_nspaces(nspaces);
   
-  flickcurl_free_photo(photo);
-
   if(fsf->emit_finish)
     fsf->emit_finish(fcs->data);
   
