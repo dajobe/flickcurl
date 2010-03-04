959fbbeee5c1b179949352e8a31d6e8219b9e7d7
Dave Beckett <dave@dajobe.org>
2010-03-03 22:10:50 -0800

    docs

--- src/stats-api.c
+++ src/stats-api.c
@@ -1094,11 +1094,12 @@ flickcurl_stats_getPopularPhotos(flickcurl* fc, const char* date,
 /**
  * flickcurl_stats_getTotalViews:
  * @fc: flickcurl context
- * @date: Stats will be returned for this date. This should be in either be in YYYY-MM-DD or unix timestamp format.
-
-A day according to Flickr Stats starts at midnight GMT for all users, and timestamps will automatically be rounded down to the start of the day.
-
-If no date is provided, all time view counts will be returned. (or NULL)
+ * @date: Stats will be returned for this date. This should be in
+ * either be in YYYY-MM-DD or unix timestamp format.  A day according
+ * to Flickr Stats starts at midnight GMT for all users, and
+ * timestamps will automatically be rounded down to the start of the
+ * day.  If no date is provided, all time view counts will be
+ * returned. (or NULL)
  * 
  * Get the overall view counts for an account
  *
