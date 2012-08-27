27f4e49f5fcff5fe35ff7b7e175afa95dbe6ad12
Dave Beckett <dave@dajobe.org>
2012-08-27 08:43:09 -0700

    2012

--- src/flickcurl_internal.h
+++ src/flickcurl_internal.h
@@ -4,7 +4,7 @@
  *
  * All API calls and defines here many change in any release.
  *
- * Copyright (C) 2007-2009, David Beckett http://www.dajobe.org/
+ * Copyright (C) 2007-2012, David Beckett http://www.dajobe.org/
  * 
  * This file is licensed under the following three licenses as alternatives:
  *   1. GNU Lesser General Public License (LGPL) V2.1 or any newer version
